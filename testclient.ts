6692e6d3e76eee0de6b2689bb784ac43b2485903
suke <akitochuanshang@gmail.com>
2024-06-14 14:41:54 +0900

    ワンライナ―で一応対応

--- testclient.ts
+++ testclient.ts
@@ -1,7 +1,9 @@
 import { readFileSync } from "fs";
+import { parseCommandLine } from "typescript";
 import net from "net";
 import readline from "readline";
-
+import commandLineArgs from 'command-line-args';
+import { error } from "console";
 interface SocketType {
     datatype: "add" | "rm" | "list";
     name?: string;
@@ -88,8 +90,88 @@ async function getPort(msg: string): Promise<number> {
 }
 
 async function entry() {
-    if (process.argv[3]) {
+    if (process.argv[2]) {
         // 引数がある場合
+        switch (process.argv[2]) {
+            case "add":
+                function testhost(ip: string) {
+                    if (ip === "host") {
+                        return "172.35.0.1"
+                    } else {
+                        return ip;
+                    }
+                }
+                const args = {
+                    name: process.argv[3],
+                    cport: process.argv[4],
+                    lport: process.argv[5],
+                    addr: process.argv[6],
+                    proto: process.argv[7]
+                }
+
+                try {
+
+                    if (Number(args.cport) > 25565 || Number(args.lport) > 25565) {
+                        throw error("IPがおかしいよ");
+                    }
+                    let istcp = false;
+                    if (args.proto === "t") { istcp = true } else {
+                        if (args.proto !== "u") {
+                            console.log(args.proto);
+                            throw error("Protocolがおかしいよ");
+                        }
+                    }
+
+                    client.write(JSON.stringify({
+                        datatype: "add",
+                        name: args.name,
+                        TCP: istcp,
+                        ip: testhost(args.addr),
+                        cport: args.cport,
+                        lport: args.lport
+                    }));
+                } catch (error) {
+                    console.log("なんか引数おかしくない？");
+                    process.exit();
+                }
+
+
+                break;
+            case "rm":
+                if (process.argv[3]) {
+                    client.write(JSON.stringify({
+                        datatype: "rm",
+                        name: process.argv[3]
+                    }));
+                } else {
+                    console.log("引数なんかおかしくない？");
+                    process.exit();
+                }
+                break;
+            case "list":
+                client.write(JSON.stringify({
+                    datatype: "list",
+                }));
+                break;
+            case "help":
+                console.log(`ワンライナー書式\n
+                    list そのまま利用可能
+                    rm [削除したいエントリ名]
+                    add name cport lport addr proto(t[cp] or u[dp])
+                    `);
+                process.exit();
+                // name: process.argv[3],
+                // cport: process.argv[4],
+                // lport: process.argv[5],
+                // addr: process.argv[6],
+                // proto: process.argv[7]
+                break;
+            default:
+                console.log("なにその引数\n俺知らないんだけど");
+                process.exit();
+                break;
+        }
+
     } else {
         // 引数がない場合
         const useractions = await question("何がしたいん？(add,rm,list): ");
